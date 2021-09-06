cfd13c8f1495166c04ab8eb6d71e1b054df626ba
Sotiris <36128807+SotirisKar@users.noreply.github.com>
2021-09-06 12:10:56 +0300

    Add files via upload

---
+++ src/Fire.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
+};
+
+// Initialize Firebase
+const fire = initializeApp(firebaseConfig);
+
+export default fire;
